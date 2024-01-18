20ae9a773b8fbdc5532c67a73b1579e8c8d98846
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-01-18 17:48:04 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.15 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.js
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.js
@@ -1,5 +1,30 @@
 function solution(n) {
     var answer = 0;
     answer = Number.isInteger(Math.sqrt(n)) === true ? 1 : 2
+    // answer = Math.sqrt(n) % 1 === 0 ? 1: 2
+    // answer = Math.sqrt(n) % 1 ? 2 : 1
+    // answer = Math.sqrt(n) === Math.floor(Math.sqrt(n)) ? 1 : 2
+
+    console.log(answer)
+    console.log( !!(Math.sqrt(n) % 1))
     return answer;
 }
+solution(144)
+
+function solution2(n) {
+    var answer = 0;
+
+    for(var i=0; i*i <= n; i++){
+        answer = i*i === n ? 1 : 2
+    }
+
+    console.log(answer)
+    return answer;
+}
+solution2(144)
+
+
+
+/**
+ * Math.sqrt()
+ */
