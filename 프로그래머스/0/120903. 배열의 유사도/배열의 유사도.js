7aaf3c227c7fb015dbaba4f01f581a09a1fe473c
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-01-18 15:52:52 +0900

    [level 0] Title: 배열의 유사도, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,5 +1,35 @@
 function solution(s1, s2) {
     var answer = 0;
     answer = s1.filter((v) => s2.includes(v)).length
+    console.log(answer)
     return answer;
 }
+solution(["a", "b", "c"], ["com", "b", "d", "p", "c"])
+
+function solution2(s1, s2) {
+    var answer = 0;
+    for(let v of s1) s2.includes(v) ? answer++ : ''
+    console.log(answer)
+    return answer;
+}
+solution2(["a", "b", "c"], ["com", "b", "d", "p", "c"])
+
+function solution3(s1, s2) {
+    var answer = 0;
+    var concat = [...s1, ...s2];
+    var setConcat = Array.from(new Set(concat))
+    answer = concat.length - setConcat.length
+    // var setConcat = new Set(concat)
+    // answer = concat.length - setConcat.size
+
+    console.log(concat, setConcat)
+    console.log(answer)
+    return answer;
+}
+solution3(["a", "b", "c"], ["com", "b", "d", "p", "c"])
+
+/**
+ * filter(), includes()
+ * for ... of
+ * new Set(), Array.from(), size
+ */
