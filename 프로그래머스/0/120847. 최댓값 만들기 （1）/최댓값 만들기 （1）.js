d24f3874b8948720f433021033373b4dc23b898c
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-01-18 14:50:49 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.17 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -1,8 +1,61 @@
 function solution(numbers) {
     var answer = 0;
-    var arr = numbers.sort((a,b) => {
-        return b-a
-    })
-    answer = arr[0] * arr[1]
+    // numbers.sort((a,b) => b - a)
+    // numbers.sort((a,b) => { return b - a })
+    numbers.sort((a,b) => a - b).reverse()
+    answer = numbers[0] * numbers[1]
+
+    console.log(answer)
     return answer;
 }
+solution([2,5,8,0,29,32])
+
+function solution2(numbers) {
+    var answer = 0;
+    let [a, b] = numbers.sort((a,b) => b - a);
+    answer = a * b
+
+    console.log(answer)
+    return answer;
+}
+solution2([2,5,8,0,29,32])
+
+function solution3(numbers) {
+    var answer = 0;
+    numbers.sort((a,b) => a - b)
+    console.log(numbers)
+    answer = numbers.at(-1) * numbers.at(-2)
+
+    console.log(answer)
+    return answer;
+}
+solution3([2,5,8,0,29,32])
+
+function solution4(numbers) {
+    var answer = 0;
+    numbers.sort((a,b) => a - b)
+    answer = numbers.pop() *  numbers.pop()
+    
+    console.log(answer)
+    return answer;
+}
+solution4([2,5,8,0,29,32])
+
+function solution5(numbers) {
+    var answer = 0;
+    let a = Math.max(...numbers)
+    numbers.splice(numbers.indexOf(a),1)
+    let b = Math.max(...numbers)
+    answer = a * b
+    
+    console.log(answer)
+    return answer;
+}
+solution5([2,5,8,0,29,32])
+
+
+/**
+ * sort(), reverse(), at(), pop()
+ * () => {return } , () =>
+ * Math.max(), splice()
+ */
