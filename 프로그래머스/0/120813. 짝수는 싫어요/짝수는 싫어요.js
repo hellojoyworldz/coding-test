fa20558d9e2edabb13b9de03f73b26f4896a8a7c
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-01-18 17:54:27 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.32 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,10 +1,17 @@
 function solution(n) {
     var answer = [];
+    for(var i = 1; i <= n; i++) i%2 !== 0 ? answer.push(i) : ''
 
-    for(var i = 1; i <= n; i++){
-        i%2 !== 0 ? answer.push(i) : ''
-    }
+    console.log(answer)
     return answer;
 }
-
 solution(15)
+
+function solution2(n) {
+    var answer = [];
+    for(var i = 1; i <= n; i+=2) answer.push(i)
+
+    console.log(answer)
+    return answer;
+}
+solution2(15)
