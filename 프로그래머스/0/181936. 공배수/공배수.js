8e8df829f349249c210811b34393807550a561a6
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-01-19 12:37:55 +0900

    [level 0] Title: 공배수, Time: 0.18 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/0/181936. 공배수/공배수.js
+++ 프로그래머스/0/181936. 공배수/공배수.js
@@ -1,5 +1,19 @@
 function solution(number, n, m) {
     var answer = 0;
-    answer = number % n === 0 && number % m === 0 ? 1 : 0
-    return answer;
+    answer = number % n === 0 && number % m === 0 ? 1: 0
+    // answer = !(number % n) && !(number % m) ? 1: 0
+
+    console.log(answer)
+    return answer
 }
+solution(10, 2, 10)
+
+
+function solution2(number, n, m) {
+    var answer = 0;
+    answer = +!(number % n || number % m);
+
+    console.log(answer)
+    return answer
+}
+solution2(10, 2, 10)
