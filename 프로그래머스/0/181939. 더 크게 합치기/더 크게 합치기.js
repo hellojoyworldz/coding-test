42b755e9bb9bfacb7134bc63c97a561c7b3202ab
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-01-22 01:15:57 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.18 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
@@ -3,12 +3,26 @@ function solution(a, b) {
     var aString = a.toString();
     var bString = b.toString();
 
-    var ab = Number(aString + bString);
-    var ba = Number(bString + aString);
-
-    answer = ab > ba ? ab : ba
+    var ab = aString + bString;
+    var ba = bString + aString;
+    // var ab = ''+a+b
+    // var ba = ''+b+a
+    
+    // answer = ab > ba ? +ab : +ba
+    // answer = ab > ba ? parseInt(ab) : parseInt(ba)
+    answer = ab > ba ? Number(ab) : Number(ba)
 
     console.log(answer)
     return answer;
 }
+solution(89,8)
 
+
+function solution2(a, b) {
+    var answer = 0;
+    answer = Math.max(`${a}${b}` , `${b}${a}`)
+
+    console.log(answer)
+    return answer;
+}
+solution2(89,8)
