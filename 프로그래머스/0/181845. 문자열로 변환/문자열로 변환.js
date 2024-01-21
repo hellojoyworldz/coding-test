028229385f25190bc1ca2377416cada754b0b574
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-01-22 01:31:42 +0900

    [level 0] Title: 문자열로 변환, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181845. 문자열로 변환/문자열로 변환.js
+++ 프로그래머스/0/181845. 문자열로 변환/문자열로 변환.js
@@ -1,5 +1,14 @@
 function solution(n) {
     var answer = '';
     answer = n.toString()
+    // answer = `${n}`
+    // answer = ''+n
+
+    console.log(answer)
     return answer;
 }
+
+solution(123)
+
+const solution2 = String
+solution2(123)
