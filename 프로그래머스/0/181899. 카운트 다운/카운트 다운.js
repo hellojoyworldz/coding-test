aa77f191ccaa7930e328fc2c26df81a08778667f
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-01-23 17:53:27 +0900

    [level 0] Title: 카운트 다운, Time: 0.28 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.js
@@ -0,0 +1,11 @@
+function solution(start, end_num) {
+  var answer = [];
+  for(var i = start; i >= end_num; i--){
+      answer.push(i)
+  }
+
+  console.log(answer)
+  return answer;
+}
+
+solution(10,3)
