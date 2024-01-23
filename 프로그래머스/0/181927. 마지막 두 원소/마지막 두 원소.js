a5c352f64394f084844e4f8b0edfb3a39764b3e0
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-01-23 17:50:20 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.15 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,3 +1,4 @@
+console.time('solution')
 function solution(num_list) {
   var answer = [];
   answer.push(...num_list)
@@ -11,5 +12,18 @@ function solution(num_list) {
   console.log(answer)
   return answer;
 }
-
 solution([5, 2, 1, 7, 5])
+console.timeEnd('solution')
+
+
+console.time('solution2')
+function solution2(num_list) {
+  var answer = [];
+  const [a, b] = [...num_list].reverse();
+  answer = [...num_list, a > b ? a-b : a*2]
+
+  console.log(answer)
+  return answer
+}
+solution2([5, 2, 1, 7, 5])
+console.timeEnd('solution2')
