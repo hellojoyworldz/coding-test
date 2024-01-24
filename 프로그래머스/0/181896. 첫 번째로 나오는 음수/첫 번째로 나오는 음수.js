e24cc802596e28bdb2303a3eec14602c65d15d40
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-01-24 18:04:32 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.12 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -13,3 +13,24 @@ function solution(num_list) {
   return answer;
 }
 solution([12, 4, 15, 46, 38, -2, 15])
+
+function solution2(num_list){
+  var answer = 0;
+  answer = num_list.findIndex(v => v < 0)
+  
+  console.log(answer)
+  return answer
+}
+solution2([12, 4, 15, 46, 38, -2, 15])
+
+function solution3(num_list){
+  var answer = 0;
+  answer = num_list.filter((v,i) =>{ 
+    if(v < 0)  return v
+  })
+  answer = num_list.indexOf(answer[0])
+  
+  console.log(answer)
+  return answer
+}
+solution3([12, 4, 15, 46, 38, -2, 15])
