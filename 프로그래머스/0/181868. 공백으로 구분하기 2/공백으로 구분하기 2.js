4404fa58e0c6d873aa60150a8b394ec5ce0665fd
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-02-10 21:34:23 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
@@ -1,6 +1,9 @@
 function solution(my_string) {
     var answer = [];
     
-    answer = my_string.trim().split(/\s+/)
+    // my_string.split(" ").map(a => {if(a !== '') answer.push(a)})
+    // answer = my_string.trim().split(/\s+/)
+    answer = my_string.split(' ').filter(v=>v)
+    
     return answer;
 }
