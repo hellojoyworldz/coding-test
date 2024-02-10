a55dd2f3dc10fd95ea7658baa91c9ebf856e5e1b
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-02-10 20:17:23 +0900

    [level 0] Title: A 강조하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.js
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.js
@@ -1,13 +1,16 @@
 function solution(myString) {
     var answer = '';
     
-    // answer = myString.toLowerCase().replaceAll("a","A")
-    
+    answer = myString.toLowerCase().replaceAll("a","A")
+    return answer;
+}
+
+function solution2(myString) {
+    var answer = '';
+
     for(let v of myString){
         if( v === 'a' || v === 'A') answer += "A"
         else answer += v.toLowerCase()
-    }
-    
-    
+    }   
     return answer;
 }
