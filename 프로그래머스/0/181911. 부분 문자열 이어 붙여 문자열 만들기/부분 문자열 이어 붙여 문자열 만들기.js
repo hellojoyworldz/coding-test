b1f9ee43167402ab3a27a163ea368ca5ef36cd53
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-02-11 23:53:28 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 3.61 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.js
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.js
@@ -1,8 +1,14 @@
 function solution(my_strings, parts) {
     var answer = '';
     
+    // answer = parts.map(([start, end], index) => my_strings[index].substring(start,end+1)).join("")
+    // answer = parts.map(([start, end], index) => my_strings[index].slice(start,end+1)).join("")
+    // answer = my_strings.reduce((res, curr, idx) => res + curr.slice(parts[idx][0], parts[idx][1]+1),'')
+    // answer = my_strings.map((v,i) => v.slice(parts[i][0],parts[i][1]+1)).join('')
     my_strings.map((v,i) => {
-        answer += v.slice(parts[i][0], parts[i][1]+1)
+        answer += v.substring(parts[i][0], parts[i][1]+1)
     })
+
+    console.log(answer)
     return answer;
 }
