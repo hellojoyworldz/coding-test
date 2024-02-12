c0ebe43a9a5fcb88fe81c0c17238d427118b8325
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-02-12 17:56:47 +0900

    [level 0] Title: ad 제거하기, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181870. ad 제거하기/ad 제거하기.js
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.js
@@ -1,6 +1,7 @@
 function solution(strArr) {
     var answer = [];
     
-    answer = strArr.filter((v) => !v.includes('ad'))
+    //answer = strArr.filter((v) => !v.includes('ad'))
+    answer = strArr.filter((v) => v.indexOf("ad") === -1)
     return answer;
 }
