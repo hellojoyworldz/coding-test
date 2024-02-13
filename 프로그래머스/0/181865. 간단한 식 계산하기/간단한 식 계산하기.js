ca70d3f3d7d86916edc9dee442c6e9a65731c8a0
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-02-13 12:48:25 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.13 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
@@ -1,15 +1,57 @@
 function solution(binomial) {
-    var answer = 0;
-  
-    let res = binomial.split(" ")
-    let a = Number(res[0])
-    let b = Number(res[2])
-  
-    if(res[1] === '+') answer = a + b
-    else if(res[1] === '-') answer = a - b
-    else if(res[1] === '*') answer = a * b
+  var answer = 0;
 
-    return answer;
+  let res = binomial.split(" ")
+  let a = Number(res[0])
+  let b = Number(res[2])
+  let op = res[1]
+
+  function ops(a,op,b){
+    if(op === '+') return a + b
+    else if(op === '-') return a - b
+    else if(op === '*') return a * b
+  }
+
+  answer = ops(a,op,b)
+  console.log(answer)
+  return answer;
 }
-
 solution("43 + 12")
+
+
+function solution2(binomial) {
+  var answer = 0;
+
+  let [a,op,b] = binomial.split(" ")
+  const ops = {
+    "+" : (a,b) => a+b,
+    "-" : (a,b) => a-b,
+    "*" : (a,b) => a*b,
+  }
+  answer = ops[op](Number(a),Number(b))
+
+  console.log(answer)
+  return answer;
+}
+solution2("43 + 12")
+
+function solution3(binomial) {
+  var answer = 0;
+
+  let [a,op,b] = binomial.split(" ")
+  
+  switch(op){
+    case "+" : 
+      answer =  parseInt(a)+parseInt(b)
+      break;
+    case "-" : 
+      answer = a-b
+      break;
+    case "*" : 
+      answer = a*b
+      break;
+  }
+  console.log(answer)
+
+}
+solution3("43 + 12")
