10145af0466eafeb63ca13dd27df788403fa2a9b
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-02-26 01:11:29 +0900

    [level 0] Title: 1로 만들기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181880. 1로 만들기/1로 만들기.js
+++ JavaScript/프로그래머스/0/181880. 1로 만들기/1로 만들기.js
@@ -1,12 +1,16 @@
 function solution(num_list) {
     var answer = 0;
     
+    /*
     num_list.map((val) => {
         while(val > 1){
             val = val % 2 === 0 ? val / 2 : (val -1) / 2
             answer++
         }
     })
+    */
+    
+    num_list.map(val => answer += val.toString(2).length -1)
     
     
     return answer;
