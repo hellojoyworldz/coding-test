fd67060c2ab4390c760bc39f8d99310edb4c3743
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-03-04 21:53:50 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 3.83 ms, Memory: 36.1 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
+++ JavaScript/프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -1,8 +1,16 @@
 function solution(arr, queries) {
     var answer = [];
-    
-    queries.map(([s,e,k]) => {for(var i=s; i<=e; i++)if(i%k === 0) arr[i]+=1})
+
+    // queries.map(([s,e,k]) => {for(var i=s; i <=e;i++) if(i%k === 0) arr[i] += 1})
+    queries.map(([s,e,k]) => {
+        for(; s<=e;s++) {
+            !(s%k) && arr[s]++
+        }
+    })
+
+    console.log(arr)
     answer = arr
-    
     return answer;
 }
+
+solution([0, 1, 2, 4, 3],	[[0, 4, 1],[0, 3, 2],[0, 3, 3]])
