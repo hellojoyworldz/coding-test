c55e5ac3a78fcfd3905f9de8b948fe88380bf1aa
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-03-04 23:30:18 +0900

    [level 0] Title: qr code, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181903. qr code/qr code.js
+++ JavaScript/프로그래머스/0/181903. qr code/qr code.js
@@ -1,6 +1,8 @@
 function solution(q, r, code) {
     var answer = '';
     
-    code.split("").map((val,idx) => { if(idx%q === r)answer += val})
+    // code.split("").map((val,idx) => { if(idx%q === r)answer += val})
+    answer = [...code].filter((val,idx) => idx%q === r).join('')
+    
     return answer;
 }
