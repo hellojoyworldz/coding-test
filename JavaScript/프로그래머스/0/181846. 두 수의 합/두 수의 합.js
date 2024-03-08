3be28cf87ad9d0710fe7a5079b052faee63c1028
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-03-08 23:11:13 +0900

    [level 0] Title: 두 수의 합, Time: 52.34 ms, Memory: 34.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181846. 두 수의 합/두 수의 합.js
+++ JavaScript/프로그래머스/0/181846. 두 수의 합/두 수의 합.js
@@ -1,6 +1,9 @@
 function solution(a, b) {
     var answer = '';
     
+    console.log(Number.MAX_SAFE_INTEGER)
+    console.log(Number.isSafeInteger())
+
     answer = (BigInt(a) + BigInt(b)).toString()
     return answer;
 }
