74b2f4a480b257b867316b90e82fb7399d743182
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-03-09 02:00:21 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 10.22 ms, Memory: 37.1 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ JavaScript/프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -7,9 +7,9 @@ function solution(arr, queries) {
         for(var i=s; i <=e; i++) if(arr[i] > k) prev.push(arr[i]) 
         var val = prev.sort((a,b) => a-b)[0] 
         val === undefined ? answer.push(-1) : answer.push(val)
-
-        
     })
+    
+    // answer = queries.map(([s,e,k]) => arr.slice(s,e+1).filter( (n) => n > k).sort((a,b) => a-b)[0] || -1)
     
      
     
