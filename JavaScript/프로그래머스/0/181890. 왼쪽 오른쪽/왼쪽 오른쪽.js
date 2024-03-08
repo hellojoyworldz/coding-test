6bfcb44e2923d590b53fce4cda694bbea749e07b
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-03-08 22:55:41 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
+++ JavaScript/프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -2,11 +2,12 @@ function solution(str_list) {
     var answer = [];
     
     for(var i=0; i <str_list.length; i++){
+    //for(var i in str_list){ i는 문자열.
         if(str_list[i] === 'l') {
             answer = str_list.slice(0,i) 
             break
         }else if(str_list[i] === 'r') {
-            answer = str_list.slice(i+1) 
+            answer = str_list.slice(Number(i)+1) 
             break
         }
     }
