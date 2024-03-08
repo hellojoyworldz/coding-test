4ee054362187d3985204b762a22282b36504bcc0
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-03-08 23:28:25 +0900

    [level 0] Title: 조건 문자열, Time: 0.04 ms, Memory: 33.8 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181934. 조건 문자열/조건 문자열.js
+++ JavaScript/프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -8,3 +8,17 @@ function solution(ineq, eq, n, m) {
     
     return answer;
 }
+
+
+function solution2(ineq, eq, n, m) {
+    const operations = {
+        '>=': (n, m) => n >= m,
+        '<=': (n, m) => n <= m,
+        '>!': (n, m) => n > m,
+        '<!': (n, m) => n < m,
+    };
+    
+    answer = Number(operations[ineq+eq](n,m))
+    console.log(answer)
+}
+solution2("<",	"=",	20,	50)
