0b05058174a9a417762737081e4bb6760c840ab4
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-03-10 20:37:44 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.05 ms, Memory: 38.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.js
+++ JavaScript/프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.js
@@ -12,3 +12,19 @@ function solution(arr) {
     answer = arr
     return answer;
 }
+
+
+function solution2(arr) {
+    var answer = [[]];
+    var len =Math.max(arr.length, Math.max(...arr.map(v => v.length)))
+    
+    for(let val of arr) while(val.length < len) val.push(0)
+    while(arr.length < len) arr.push(Array(len).fill(0))
+
+    answer = arr
+    console.log(answer)
+    return answer;
+}
+
+solution2([[572, 22, 37], [287, 726, 384], [85, 137, 292], [487, 13, 876]])
+solution2([[57, 192, 534, 2], [9, 345, 192, 999]])
