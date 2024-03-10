3bf0c943e3e17183e3ad59feaff9a70a01fa5f14
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-03-10 23:56:24 +0900

    [level 0] Title: 그림 확대, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181836. 그림 확대/그림 확대.js
+++ JavaScript/프로그래머스/0/181836. 그림 확대/그림 확대.js
@@ -5,6 +5,7 @@ function solution(picture, k) {
         var data = ''
         for(var i=0; i < val.length; i++){
             for(var j=0; j < k; j++) data += val[i]
+            //data += val[j].repeat(k)
         }
         for(var n=0; n <k;n++) answer.push(data)      
     })
@@ -12,3 +13,15 @@ function solution(picture, k) {
     
     return answer;
 }
+
+function solution2(picture, k) {
+    var answer = [];
+
+    picture.map((val,idx) => {
+        var data = [...val].reduce((acc,cur) => acc + cur.repeat(k),'')
+        for(var n=0; n <k;n++) answer.push(data)
+    })
+    return answer;
+}
+
+solution2(["x.x", ".x.", "x.x"]	,3)
