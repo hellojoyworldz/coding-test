93e8ada976aefc7f614bb11b59953bb6fbeab5bf
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-03-11 13:05:52 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.26 ms, Memory: 33.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
+++ JavaScript/프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
@@ -1,7 +1,8 @@
 function solution(rank, attendance) {
     var answer = 0;
     
-    var [a,b,c] = rank.map((v,i) => { if(attendance[i] === true) return [v,i]} ).sort((a,b)=>a[0]-b[0])
+    // var [a,b,c] = rank.map((v,i) => { if(attendance[i] === true) return [v,i]} ).sort((a,b)=>a[0]-b[0])
+    var [a,b,c] = rank.map((v,i) => [v,i]).filter(([v,i]) => attendance[i]).sort(([a],[b]) => a-b)
     answer = (10000*a[1]) + (100*b[1]) + c[1]
     return answer;
 }
