1837b7c24d39311bfd579d573c90711156d1df09
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-04-29 23:52:35 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120837. 개미 군단/개미 군단.js
+++ JavaScript/프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -1,5 +1,11 @@
 function solution(hp) {
     var answer = 0;
+    answer = Math.trunc(hp/5) + Math.trunc(hp%5/3) +  Math.trunc(hp%5%3/1)
+    return answer;
+}
+
+function solution2(hp) {
+    var answer = 0;
 
     var i = 5
     while(hp !==0 && i >0){
