c35a5ee3780ccb4414acebdb5e7486a384890f18
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-04-30 00:40:40 +0900

    [level 0] Title: 가위 바위 보, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
+++ JavaScript/프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,6 +1,31 @@
 function solution(rsp) {
     var answer = '';
     
+    const point = {
+        2: 0,
+        0: 5,
+        5: 2,
+    }
+    
+    answer = [...rsp].map(val => point[val]).join("")
+    return answer;
+}
+
+function solution3(rsp) {
+    var answer = '';
+    
+    answer = [...rsp].map(val => (
+       val === "2" ? 0 :
+       val === "0" ? 5 :
+       val === "5" ? 2 : ''
+    )).join("")
+    return answer;
+}
+
+
+function solution2(rsp) {
+    var answer = '';
+    
     [...rsp].map(val => {
        val === "2" ? answer+=0 :
        val === "0" ? answer+=5 :
