1c298219fd9313eb42fd2ce5ebe1b60aa6b35c7e
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-05-06 20:30:33 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
+++ JavaScript/프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,6 +1,35 @@
 function solution(my_string) {
     var answer = '';
     
+   for(let i=0; i < my_string.length;i++){
+       answer += my_string.charCodeAt(i) < 97 ? my_string[i].toLowerCase() : my_string[i].toUpperCase()
+   }
+    return answer;
+}
+
+
+function solution2(my_string) {
+    var answer = '';
+    
+    answer = my_string.split("").map((val,idx) => {
+        return val.charCodeAt() < 97 ? val.toLowerCase() : val.toUpperCase()
+    }).join("")
+    return answer;
+}
+
+
+function solution3(my_string) {
+    var answer = '';
+    
+    for(let val of my_string){
+        answer += val === val.toUpperCase() ? val.toLowerCase() : val.toUpperCase()
+    }  
+    return answer;
+}
+
+function solution4(my_string) {
+    var answer = '';
+    
     my_string.split("").filter(val => {
         answer += val < "a" ? val.toLowerCase() : val.toUpperCase()
     })
