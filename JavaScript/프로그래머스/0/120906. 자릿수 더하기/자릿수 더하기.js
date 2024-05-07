4beedef135baf8f29af805f3179aeda75f9bc205
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-05-07 23:47:18 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.js
+++ JavaScript/프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -1,5 +1,11 @@
 function solution(n) {
     var answer = 0;
+    answer = n.toString().split("").reduce((acc, cur) =>  acc+Number(cur),0)
+    return answer;
+}
+
+function solution2(n) {
+    var answer = 0;
     let num = n.toString()
     for(let i=0; i < num.length ;i++) {
         answer += Number(num[i])
