24db9e08df53fd9f7ced532c9335c25d72f0a528
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-05-08 18:13:11 +0900

    [level 0] Title: 가까운 수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120890. 가까운 수/가까운 수.js
+++ JavaScript/프로그래머스/0/120890. 가까운 수/가까운 수.js
@@ -1,17 +1,38 @@
 function solution(array, n) {
-    var answer = 0;
-    
-    answer = array[0]
-    var abs = Math.abs(array[0] - n)
-    array.map((val,idx) => {
-        if(abs === Math.abs(val - n)){
-            answer = answer > val ? val : answer
-        }else if(abs > Math.abs(val - n)){
-            abs = Math.abs(val - n)
-            answer = val
-        }
-        
-    })
-    
-    return answer;
+  var answer = 0;
+
+  answer = array.sort((a,b) => Math.abs(a-n) - Math.abs(b-n) || a-b)[0]
+
+  return answer;
+}
+
+function solution2(array, n) {
+  var answer = 0;
+
+  answer = array[0];
+  var abs = Math.abs(array[0] - n);
+  array.map((val, idx) => {
+    if (abs === Math.abs(val - n)) {
+      answer = answer > val ? val : answer;
+    } else if (abs > Math.abs(val - n)) {
+      abs = Math.abs(val - n);
+      answer = val;
+    }
+  });
+
+  return answer;
+}
+
+function solution3(array, n) {
+  var answer = 0;
+
+  array.map((val, idx) => {
+    if (Math.abs(answer - n) > Math.abs(val - n)) {
+      answer = val;
+    } else if (Math.abs(answer - n) === Math.abs(val - n)) {
+      answer = answer > val ? val : answer;
+    }
+  });
+
+  return answer;
 }
