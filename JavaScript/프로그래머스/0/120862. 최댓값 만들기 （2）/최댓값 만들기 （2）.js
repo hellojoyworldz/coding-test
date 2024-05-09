783bfd47fc4ee5920d214e234551a99e06b563ac
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-05-10 00:31:05 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ JavaScript/프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,4 +1,17 @@
+
 function solution(numbers) {
+    var answer = 0;
+    
+    numbers.sort((a,b) => a-b)
+    
+    answer = Math.max(numbers[0] * numbers[1],numbers[numbers.length-2] * numbers[numbers.length-1])
+
+    return answer;
+}
+
+
+
+function solution2(numbers) {
     var answer = 0;
     
     numbers.sort((a,b) => a-b)
@@ -6,7 +19,7 @@ function solution(numbers) {
     let fir = numbers[0] * numbers[1]
     let lst = numbers[numbers.length-2] * numbers[numbers.length-1]
     
-    answer = fir > lst ? fir : lst
+    answer = fir >= lst ? fir : lst
     
     return answer;
 }
