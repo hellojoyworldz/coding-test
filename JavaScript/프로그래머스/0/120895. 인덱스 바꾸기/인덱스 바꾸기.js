bfb39209018ea2dfe452bde61cd32fcfcfcd0a87
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-05-12 12:19:21 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ JavaScript/프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,12 +1,22 @@
 function solution(my_string, num1, num2) {
     var answer = '';
     
+    let ary = my_string.split('');
+    [ary[num1], ary[num2]] =  [ary[num2], ary[num1]];
+    answer = ary.join('')
+
+    return answer;
+}
+
+function solution2(my_string, num1, num2) {
+    var answer = '';
+    
     answer = my_string.split("").map((val,idx) => {
         if(idx === num1) return my_string[num2]
         else if(idx === num2) return my_string[num1]
         else return val
-    })
+    }).join("")
     
-    answer = answer.join("")
+
     return answer;
 }
