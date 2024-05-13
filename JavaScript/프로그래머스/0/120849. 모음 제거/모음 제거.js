bf5977037bb614080476212ba54dfd8b79639f30
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-05-13 11:55:00 +0900

    [level 0] Title: 모음 제거, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120849. 모음 제거/모음 제거.js
+++ JavaScript/프로그래머스/0/120849. 모음 제거/모음 제거.js
@@ -1,9 +1,20 @@
 function solution(my_string) {
     var answer = '';
  
+    answer = Array.from(my_string).filter((val) => !["a","e","i","o","u"].includes(val)).join("")
+    
+    return answer;
+}
+
+
+function solution2(my_string) {
+    var answer = '';
+ 
     answer = my_string.split("").filter((val) => {
         if(val !== "a" && val !== "e" && val !== "i" && val !== "o" && val !== "u") return val
     }).join("")
     
     return answer;
 }
+
+
