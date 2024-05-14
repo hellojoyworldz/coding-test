ef55e8e10773594d612495957f70a6953f210943
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-05-14 21:53:16 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
+++ JavaScript/프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,4 +1,17 @@
+
 function solution(numbers, direction) {
+    var answer = [];
+    
+    answer = direction === "right"
+    ? [numbers[numbers.length - 1], ...numbers.slice(0, numbers.length - 1)]
+    : [...numbers.slice(1), numbers[0]];
+    
+    return answer;
+}
+
+
+
+function solution2(numbers, direction) {
     var answer = [];
     let num ;
     
