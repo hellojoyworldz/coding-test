193350c2e3c6dcb229297f8aa379e0c7b2a12515
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-05-16 01:49:30 +0900

    [level 0] Title: 약수 구하기, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120897. 약수 구하기/약수 구하기.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    var answer = [];
+    
+    for(let i=1; i <= n ;i++){
+        if(n%i === 0){
+            answer.push(i)
+            answer.push(n/i)
+        }
+    }
+    
+    answer = answer.filter((val, idx, self) => self.indexOf(val) === idx).sort((a,b) => a-b)
+
+    return answer;
+}
