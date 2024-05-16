40785e15a93fe15d8017d256ae8e5803cc6fceb8
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-05-16 18:47:50 +0900

    [level 0] Title: 숫자 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120904. 숫자 찾기/숫자 찾기.js
+++ JavaScript/프로그래머스/0/120904. 숫자 찾기/숫자 찾기.js
@@ -1,6 +1,14 @@
 function solution(num, k) {
     var answer = 0;
     
+    answer = num.toString().indexOf(k)+1 || -1
+
+    return answer;
+}
+
+function solution2(num, k) {
+    var answer = 0;
+    
     answer = num.toString().indexOf(k)
     if(answer !== -1) answer += 1
 
