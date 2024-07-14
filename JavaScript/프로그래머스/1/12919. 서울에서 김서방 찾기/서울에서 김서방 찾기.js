892231aaf292e648939b89ff2b6d484c541caccb
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-07-14 20:06:52 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ JavaScript/프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,13 +1,19 @@
 function solution(seoul) {
     var answer = '';
     
+    answer  =  "김서방은 " + seoul.indexOf("Kim") + "에 있다"
+    
+    return answer;
+}
+
+function solution2(seoul) {
+    var answer = '';
+    
     seoul.forEach((val,x) => {
         if(val === "Kim") {
             answer = `김서방은 ${x}에 있다`;
             return;
         }
     })
-    
-    
     return answer;
 }
