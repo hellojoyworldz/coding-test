011dc84cf2e4d3049eab56b93be33b7506d6dd41
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-07-23 23:16:36 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 4.43 ms, Memory: 43.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -0,0 +1,15 @@
+function solution(arr) {
+    var answer = [];
+    
+    if(arr.length === 1) {
+        answer = [-1]
+    }else{
+        var min = Math.min(...arr)
+        answer = arr.filter(val => val !== min)
+    }
+    
+    
+    
+    
+    return answer
+}
