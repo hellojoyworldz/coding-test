636d599a40df5aaf27a50199a678dcaee56da64d
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-09-03 13:27:14 +0900

    [level 0] Title: 분수의 덧셈, Time: 676.43 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -7,19 +7,14 @@ function solution(numer1, denom1, numer2, denom2) {
     
     
     for(var i=2; i <= Math.min(numer, denom); i++){
-        if(numer % i ===0 && denom %i ===0){
-            ary.push(i)
-        }
+        if(numer % i ===0 && denom %i ===0) ary.push(i)
     }
     
     let len = ary.length
-    if(ary.length === 0){
-        answer = [numer, denom]
-    }else{
-        answer = [numer/ary[len-1], denom/ary[len-1]]
-    }
+    answer = ary.length === 0 ? [numer, denom] : [numer/ary[len-1], denom/ary[len-1]]
  
     
     return answer;
 }
+
 
