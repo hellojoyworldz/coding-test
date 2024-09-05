5cd0a12fc4847973cbcf3669135526e6b7d18d8c
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-09-05 11:41:10 +0900

    [level 2] Title: 올바른 괄호, Time: 27.01 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -4,20 +4,11 @@ function solution(s){
     if(s[0] === ")" || s[s.length -1] === "(") return false
 
     var num =0;
-    for(var i=0; i < s.length;i++){
-        if(s[i] === "("){
-            num += 1
-        }else{
-            num -= 1
-            if(num < 0){
-                answer = false;
-                break;
-            }
-        }
+    for(var txt of s){
+        num += txt === "(" ? 1 : -1
+        if(num <0) return false
     }
-    
-    answer = num === 0 ? true: false
 
-    
+    answer = num === 0 ? true: false
     return answer;
 }
