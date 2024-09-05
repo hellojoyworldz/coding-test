9757a413a941642826a24a9441789824cfd911e5
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-09-05 11:33:06 +0900

    [level 2] Title: 올바른 괄호, Time: 22.83 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,21 @@
+function solution(s){
+    var answer = true;
+
+    var num =0;
+    for(var i=0; i < s.length;i++){
+        if(s[i] === "("){
+            num += 1
+        }else{
+            num -= 1
+            if(num < 0){
+                answer = false;
+                break;
+            }
+        }
+    }
+    
+    answer = num === 0 ? true: false
+
+    
+    return answer;
+}
