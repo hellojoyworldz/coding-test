e513431c5c2651405453e76b2d31ec7e96c631e0
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-09-09 19:31:00 +0900

    [level 1] Title: 예산, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,19 +1,15 @@
 function solution(d, budget) {
-    var ary = d.sort((a,b) => a-b)
+
     var answer = 0;
     var hap = 0;
     
-    for(var i=0; i < ary.length;i++){
-        hap += ary[i]
+    d.sort((a,b) => a-b).forEach(val => {
+        hap += val
         
-        if(hap <= budget){
+        if(hap <= budget) {
             answer+=1
-        }else if(hap > budget){
-           break
         }
-        
-    }
-    
+    })
 
     
     return answer;
