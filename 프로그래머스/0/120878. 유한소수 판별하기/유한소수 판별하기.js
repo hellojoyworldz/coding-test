c6ed69a3a116d12a8a0046a0488fd4d17ad5cb60
GwakSunah <116714457+hellojoyworldz@users.noreply.github.com>
2024-09-18 23:16:33 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -1,5 +1,28 @@
 function solution(a, b) {
     var answer = 0;
+    
+    if(a === b) return 2;
+    if(a % b === 0) return 1;
+    
+    var max = 1;
+    for(var i=1; i <= Math.min(a,b); i++){
+        if(a%i === 0 && b%i === 0) max = i;
+    }
+    
+    b /= max
+    
+    while(b % 2 === 0) b/=2
+    while(b % 5 === 0) b/=5
+
+    answer = b === 1 ? 1 : 2
+    
+    return answer
+    
+    
+}
+
+function solution2(a, b) {
+    var answer = 0;
     var max = 1;
     
     if(a === b) return 2;
